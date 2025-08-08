ea822e481b02e9e9f8bcdd8654c4cc2cb6f905ec
jstevenskc <111237420+jstevenskc@users.noreply.github.com>
2025-08-09 02:14:39 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,48 @@
+resource "aws_vpc" "terra_vpc" {
+    cidr_block = "10.0.0.0/16"
+    instance_tenancy = "default"
+
+    tags = {
+        Name = "jon-vpc"
+    }
+}
+
+variable "names" {
+  default = ["jon-sn1","jon-sn2","jon-sn3"]
+}
+
+resource "aws_subnet" "terra_subnet" {
+  vpc_id     = aws_vpc.terra_vpc.id
+  cidr_block = "10.0.0.0/24"
+
+ for_each = toset(var.names)
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_route_table" "terra_rt" {
+  vpc_id = aws_vpc.terra_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terra_igw.id
+  }
+
+  tags = {
+    Name = "jon-rt"
+  }
+}
+
+resource "aws_internet_gateway" "terra_igw" {
+  vpc_id = aws_vpc.terra_vpc.id
+
+  tags = {
+    Name = "jon-igw"
+  }
+}
+
+resource "aws_route_table_association" "terra_association" {
+  subnet_id      = aws_subnet.terra_subnet.id
+  route_table_id = aws_route_table.terra_rt.id
+}
